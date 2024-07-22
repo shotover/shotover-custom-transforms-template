d11d182dfb6fb7861bd85cf9af1fda097e32815e
Lucas Kent <rubickent@gmail.com>
2024-07-22 14:11:19 +1000

    Update to shotover 0.4

--- shotover-bin/tests/test.rs
+++ shotover-bin/tests/test.rs
@@ -8,23 +8,21 @@ use tokio_bin_process::event_matcher::EventMatcher;
 use tokio_bin_process::{bin_path, BinProcess};
 
 fn docker_compose(yaml_path: &str) -> DockerCompose {
-    DockerCompose::new(
-        &[
-            Image {
-                name: "library/redis:5.0.9",
-                log_regex_to_wait_for: r"Ready to accept connections",
-                timeout: 120,
-            },
-            Image {
-                name: "bitnami/kafka:3.4.0-debian-11-r22",
-                log_regex_to_wait_for: r"Kafka Server started",
-                timeout: 120,
-            },
-        ],
-        |_| {},
-        yaml_path,
-    )
+    DockerCompose::new(&IMAGE_WAITERS, |_| {}, yaml_path)
 }
+
+pub static IMAGE_WAITERS: [Image; 2] = [
+    Image {
+        name: "library/redis:5.0.9",
+        log_regex_to_wait_for: r"Ready to accept connections",
+        timeout: Duration::from_secs(120),
+    },
+    Image {
+        name: "bitnami/kafka:3.4.0-debian-11-r22",
+        log_regex_to_wait_for: r"Kafka Server started",
+        timeout: Duration::from_secs(120),
+    },
+];
 
 async fn shotover(topology_path: &str) -> BinProcess {
     let mut shotover = BinProcess::start_binary(
